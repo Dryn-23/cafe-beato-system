9f49ab4eb19b58ed4e7fddc559c2cba22a3ce4ff
Driyahn_23 <edriantzy23@gmail.com>
2026-09-04 16:33:41 +0800

    online

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = " https://cafe-beato-system.onrender.com";
 
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
