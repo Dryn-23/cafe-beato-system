54de64bf0d76e7931a22188f905b4e9d48645a8d
Driyahn_23 <edriantzy23@gmail.com>
2026-09-04 17:10:25 +0800

    local2

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://cafe-beato-system.onrender.com"; // Replace with your backend API URL
+const API_URL = "https://cafe-beato-system-2.onrender.com"; // Replace with your backend API URL
 
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
